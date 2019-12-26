f8fb8e7887779c098f0e97d1595441965bcdf9a1
Erlend Ellingsen <erlend.ame@gmail.com>
2019-12-26 11:45:26 +0100

    badges wip

--- app.go
+++ app.go
@@ -9,18 +9,22 @@ type Player struct {
 }
 
 type Activity struct {
-	cardio   bool
-	distance float32
+	cardio bool
 }
 
 type ActivitiesStruct struct {
 	skiing_xc Activity
 }
 
+type ActivityEntry struct {
+	activity    Activity
+	outsiteTemp float32
+	distance    float32
+}
+
 var Activities = ActivitiesStruct{
 	skiing_xc: Activity{
-		cardio:   false,
-		distance: 5.0,
+		cardio: false,
 	},
 }
 
@@ -32,16 +36,19 @@ func (p Player) AddRank(newProg float32) Player {
 		p.level++
 		p.progress = 0
 		surplusProgress := newProgress - targetProgress
+		fmt.Println("Player level up", p.level, "surplus progress", surplusProgress)
 		if surplusProgress > 0 {
 			return p.AddRank(surplusProgress)
 		}
+	} else {
+		p.progress = newProgress
+		fmt.Println("Player level", p.level, "current progress", p.progress)
 	}
 
 	return p
 }
 
-func (p Player) AddActivity(a Activity) Player {
-
+func (p Player) AddActivity(a ActivityEntry) Player {
 	progressFromActivity := a.distance * 1.2
 	return p.AddRank(progressFromActivity)
 }
@@ -58,12 +65,17 @@ func main() {
 		},
 	}
 
-	fmt.Println(p)
+	// fmt.Println(p)
 
-	p = p.AddActivity(Activities.skiing_xc)
+	act := ActivityEntry{
+		activity:    Activities.skiing_xc,
+		distance:    16.0,
+		outsiteTemp: -1.0,
+	}
 
-	fmt.Println(p)
+	p = p.AddActivity(act)
+	p = p.ProcessBadges(act)
 
-	processBadges(Activities.skiing_xc, p)
+	// fmt.Println(p)
 
 }
--- badges.go
+++ badges.go
@@ -2,22 +2,54 @@ package main
 
 import "fmt"
 
+type TemperatureRequirement struct {
+	temperature float32
+	greaterThan bool
+}
+
+type SingleActivityRequirement struct {
+	distance                   float32
+	hasTemperatureRequirement  bool
+	temperature                TemperatureRequirement
+	hasActivityTypeRequirement bool
+	activityTypeRequirement    Activity
+}
+
 type Badge struct {
-	id   uint32
-	name string
-	desc string
+	id                           uint32
+	name                         string
+	desc                         string
+	hasSingleActivityRequirement bool
+	singleActivityRequirement    SingleActivityRequirement
 }
 
 var Badges = []Badge{
 	Badge{
-		id:   0x1,
-		name: "dfe",
-		desc: "gjjk",
+		id:                           0x1,
+		name:                         "Hello winter",
+		desc:                         "Complete an activity in freezing temperatures",
+		hasSingleActivityRequirement: true,
+		singleActivityRequirement: SingleActivityRequirement{
+			distance:                  0.0,
+			hasTemperatureRequirement: true,
+			temperature: TemperatureRequirement{
+				greaterThan: false,
+				temperature: 0.0,
+			},
+		},
 	},
 	Badge{
-		id:   0x2,
-		name: "asd",
-		desc: "desc",
+		id:                           0x2,
+		name:                         "Skier basic boomer",
+		desc:                         "Ski for at least 5km",
+		hasSingleActivityRequirement: true,
+		singleActivityRequirement: SingleActivityRequirement{
+			hasActivityTypeRequirement: true,
+			activityTypeRequirement:    Activities.skiing_xc,
+			distance:                   5.0,
+			hasTemperatureRequirement:  false,
+			temperature:                TemperatureRequirement{},
+		},
 	},
 	Badge{
 		id:   0x3,
@@ -26,13 +58,14 @@ var Badges = []Badge{
 	},
 }
 
-func processBadges(a Activity, p Player) Player {
+func (p Player) ProcessBadges(a ActivityEntry) Player {
 	// for i := range Badges {
 	// 	if Badges[i].id
 	// }
 
 	var remainingBadges []Badge
 
+	// Identify remaining badges
 	for _, v := range Badges {
 		var foundMatch = false
 		for _, vp := range p.badges {
@@ -46,7 +79,41 @@ func processBadges(a Activity, p Player) Player {
 		}
 	}
 
+	// Iterate through badges and test them
+	for _, b := range remainingBadges {
+		gotBadge := testBadgeRequirements(p, a, b)
+		if gotBadge {
+			p.badges = append(p.badges, b)
+			fmt.Println("Player was awarded badge", b.name, "[", b.desc, "]")
+		}
+	}
+
 	fmt.Println(remainingBadges)
 
 	return p
+}
+
+func testBadgeRequirements(p Player, a ActivityEntry, b Badge) bool {
+	// Process remaining badgess
+	req := b.singleActivityRequirement
+
+	activityTypeRequirement := true
+	if req.hasActivityTypeRequirement {
+		activityTypeRequirement = a.activity == req.activityTypeRequirement
+	}
+
+	// process distance
+	distOk := a.distance >= req.distance
+
+	// process temp
+	tempOk := false
+	if req.hasTemperatureRequirement {
+		if req.temperature.greaterThan {
+			tempOk = a.outsiteTemp > req.temperature.temperature
+		} else {
+			tempOk = a.outsiteTemp < req.temperature.temperature
+		}
+	}
+
+	return activityTypeRequirement && distOk && tempOk
 }
